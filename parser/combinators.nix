27b9c111f9fb807e1a40949901004b63a8cf9934
adueck <adueck@users.noreply.github.com>
2025-08-08 15:03:49 +0500

    more parser combinators

--- parser/combinators.nix
+++ parser/combinators.nix
@@ -10,11 +10,46 @@ let
         if result == false
           then orParser rest tokens
           else result;
+
   thenParser = parser: f: tokens:
     let
       res = parser tokens;
     in if res == false
       then false
       else f res.tokens;
+
+  parseChar = char: tokens: if (builtins.length tokens) == 0
+    then false
+    else let
+      first = builtins.head tokens;
+      rest = builtins.tail tokens;
+    in if first == char
+      then {
+        body = char;
+        tokens = rest;
+      }
+      else false;
+
+  mapParser = f: p: tokens:
+    let
+      res = p tokens;
+    in if res == false
+      then false
+    else {
+      tokens = res.tokens;
+      body = f res.body;
+    };
+  
+  # bindParser = parser: f: res: tokens:
+  #   let
+  #     res = parser tokens;
+  #   in if res == false
+  #     then false
+  #     else f res.tokens;
 in
-{ orParser = orParser; thenParser = thenParser; }
+{
+  orParser = orParser;
+  thenParser = thenParser;
+  parseChar = parseChar;
+  mapParser = mapParser;
+}
--- parser/parse.nix
+++ parser/parse.nix
@@ -1,54 +1,38 @@
 let
-  combinators = import ./combinators.nix;
+  combs = import ./combinators.nix;
   parseNumber = import ./parse-number.nix;
 
   # TODO: to simplify this we need a bindParser combinator
-  parseElem = combinators.thenParser eatWhiteSpace (tokens: let
+  parseElem = combs.thenParser parseWhiteSpace (tokens: let
     res = parseElem' tokens;
   in if res == false
     then false
     else {
-      tokens = (eatWhiteSpace res.tokens).tokens;
+      tokens = (parseWhiteSpace res.tokens).tokens;
       body = res.body;
     });
 
-  parseElem' = combinators.orParser [
+  parseElem' = combs.orParser [
     parseSExpr
     parseOp
     parseNumber
   ];
-
-  parseBracketStart = tokens: if (builtins.length tokens) == 0
-    then false
-    else let
-      first = builtins.head tokens;
-      rest = builtins.tail tokens;
-    in if first == "("
-      then {
-        body = true;
-        tokens = rest;
-      }
-      else false;
  
-  parseSExpr = combinators.thenParser parseBracketStart (tokens:
-    let
-      args = parseSExpr' [ ] tokens;
-    in if args == false
-      then false
-      else {
-        tokens = args.tokens;
-        body = {
-          type = "s-expr";
-          value = args.body; 
-        };
-      });
+  parseSExpr = combs.mapParser
+    (x: {
+      type = "s-expr";
+      value = x;
+    })
+    (combs.thenParser
+      (combs.parseChar "(")
+      (parseSExpr' [ ]));
 
-  eatWhiteSpace = tokens: if (builtins.length tokens) == 0
+  parseWhiteSpace = tokens: if (builtins.length tokens) == 0
     then { body = true; tokens = tokens; }
     else let
       first = builtins.head tokens;
     in if isWhiteSpace first
-      then eatWhiteSpace (builtins.tail tokens)
+      then parseWhiteSpace (builtins.tail tokens)
       else { body = true; tokens = tokens; };
 
   parseSExpr' = prev: tokens: let 
@@ -65,19 +49,16 @@ let
       (builtins.concatLists [ prev [nextArg.body] ])
       nextArg.tokens;
 
-  parseOp = tokens: let
-    first = builtins.head tokens;
-    rest = builtins.tail tokens;
-    val = if first == "+"
-      then "+"
-      else if first == "-"
-      then "-"
-      else if first == "*"
-      then "*"
-      else false;
-    in if val == false
-      then val
-      else { tokens = rest; body = { type = "op"; value = val; }; };
+  parseOp = combs.mapParser
+    (x: {
+      type = "op";
+      value = x;
+    })
+    (combs.orParser [
+      (combs.parseChar "+")
+      (combs.parseChar "-")
+      (combs.parseChar "*")
+    ]);
   
   isWhiteSpace = s: s == " " || s == "\t" || s == "\n";  
 in
