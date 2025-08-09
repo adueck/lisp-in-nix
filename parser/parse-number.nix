36a3cfe6ce3b4676ce36ca2acefa8308f5d2b00c
adueck <adueck@users.noreply.github.com>
2025-08-10 01:11:29 +0500

    refactor number parsing a bit

--- parser/parse-number.nix
+++ parser/parse-number.nix
@@ -1,6 +1,18 @@
 let
   combs = import ./combinators.nix;
   utils = import ../utils/utils.nix;
+  numTable = {
+    "0" = 0;
+    "1" = 1;
+    "2" = 2;
+    "3" = 3;
+    "4" = 4;
+    "5" = 5;
+    "6" = 6;
+    "7" = 7;
+    "8" = 8;
+    "9" = 9;
+  };
 
   parseNumber = 
     (combs.mapParser
@@ -20,25 +32,7 @@ let
 
   power = base: exp: if exp == 0 then 1 else base * power base (exp - 1);
 
-  digitCharToInt = c: if c == "1"
-    then 1
-    else if c == "2"
-    then 2
-    else if c == "3"
-    then 3
-    else if c == "4"
-    then 4
-    else if c == "5"
-    then 5
-    else if c == "6"
-    then 6
-    else if c == "7"
-    then 7
-    else if c == "8"
-    then 8
-    else if c == "9"
-    then 9
-    else 0;
+  digitCharToInt = c: builtins.getAttr c numTable;
 
 in
 parseNumber
