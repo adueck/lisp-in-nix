a50f456396a794ccac5f8ea0c8cc6b4928930292
adueck <adueck@users.noreply.github.com>
2025-08-10 00:41:33 +0500

    added more ops

--- source.lisp
+++ source.lisp
@@ -1,8 +1,7 @@
-(not 93)
 ; basic LISP syntax for calculations
-; (let
-;   ; declare variables
-;   ((x 1) (y 2))
-;   (* 
-;     (+ x y #| inline comments too |# )
-;     10))
+(let
+  ; declare variables
+  ((x 1) (y 2))
+  (* 
+    (+ x y #| inline comments too |# )
+    10))
